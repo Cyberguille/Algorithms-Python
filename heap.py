83b57c05d1c2de61a8a0dede11e8575073f6a6c9
Ramon <ramon@localhost>
2015-03-06 23:25:54 -0500

    heap OK!!!

--- heap.py
+++ heap.py
@@ -1,13 +1,14 @@
 __author__ = 'Ramon'
 
 '''
-Heap minimum property: at every node X, key[X] <= all keys of X's children.
+Heap property: at every node X, key[X] <= (if min) or >= (if max) all keys of X's children.
 '''
 
 
-class heap_min():
-    def __init__(self, array):
+class heap():
+    def __init__(self, array, hmax=False):
         self.tree = list()
+        self.max = hmax
         for i in array:
             self.insert(i)
 
@@ -20,16 +21,25 @@ class heap_min():
         self.bubble_up(value_index)
 
     def bubble_up(self, value_index):
-        parent_index = (value_index-1)//2
-        if self.tree[value_index] < self.tree[parent_index]:
-            self.tree[value_index], self.tree[parent_index] = self.tree[parent_index], self.tree[value_index]
-            self.bubble_up(parent_index)
+        if value_index > 0:
+            parent_index = (value_index-1)//2
+            if self.max is True:
+                flag = self.tree[value_index] > self.tree[parent_index]
+            else:
+                flag = self.tree[value_index] < self.tree[parent_index]
+            if flag:
+                self.tree[value_index], self.tree[parent_index] = self.tree[parent_index], self.tree[value_index]
+                self.bubble_up(parent_index)
 
     def bubble_down(self, value_index):
         child_index1 = (value_index+1)*2 - 1
         child_index2 = (value_index+1)*2
         if child_index2 < len(self.tree):
-            if self.tree[child_index1] < self.tree[child_index2]:
+            if self.max is True:
+                flag = self.tree[child_index1] > self.tree[child_index2]
+            else:
+                flag = self.tree[child_index1] < self.tree[child_index2]
+            if flag:
                 self.tree[value_index], self.tree[child_index1] = self.tree[child_index1], self.tree[value_index]
                 self.bubble_down(child_index1)
             else:
@@ -46,72 +56,18 @@ class heap_min():
         return root
 
 
-'''
-Heap maximum property: at every node X, key[X] >= all keys of X's children.
-'''
+def test():
+    c = [4, 4, 8, 9, 4, 12, 9, 11, 13]
+    h = heap(c, True)
+    print(h)
+    h.insert(7)
+    print(h)
+    h.insert(10)
+    print(h)
+    h.insert(5)
+    print(h)
+    print(h.extract())
+    print(h)
 
-
-class heap_max():
-    def __init__(self, array):
-        self.tree = list()
-        for i in array:
-            self.insert(i)
-
-    def __str__(self):
-        return str(self.tree)
-
-    def insert(self, value):
-        self.tree.append(value)
-        value_index = len(self.tree)-1
-        self.bubble_up(value_index)
-
-    def bubble_up(self, value_index):
-        parent_index = (value_index-1)//2
-        if self.tree[value_index] > self.tree[parent_index]:
-            self.tree[value_index], self.tree[parent_index] = self.tree[parent_index], self.tree[value_index]
-            self.bubble_up(parent_index)
-
-    def bubble_down(self, value_index):
-        child_index1 = (value_index+1)*2 - 1
-        child_index2 = (value_index+1)*2
-        if child_index2 < len(self.tree):
-            if self.tree[child_index1] > self.tree[child_index2]:
-                self.tree[value_index], self.tree[child_index1] = self.tree[child_index1], self.tree[value_index]
-                self.bubble_down(child_index1)
-            else:
-                self.tree[value_index], self.tree[child_index2] = self.tree[child_index2], self.tree[value_index]
-                self.bubble_down(child_index2)
-        elif child_index1 < len(self.tree):
-            self.tree[value_index], self.tree[child_index1] = self.tree[child_index1], self.tree[value_index]
-            self.bubble_down(child_index1)
-
-    def extract(self):
-        self.tree[0], self.tree[-1] = self.tree[-1], self.tree[0]
-        root = self.tree.pop()
-        self.bubble_down(0)
-        return root
-
-'''
-a = [4, 4, 8, 9, 4, 12, 9, 11, 13]
-hmin = heap_min(a)
-print(hmin)
-hmin.insert(7)
-print(hmin)
-hmin.insert(10)
-print(hmin)
-hmin.insert(5)
-print(hmin)
-print(hmin.extract())
-print(hmin)
-'''
-b = [4, 4, 8, 9, 4, 12, 9, 11, 13]
-hmax = heap_max(b)
-print(hmax)
-hmax.insert(7)
-print(hmax)
-hmax.insert(10)
-print(hmax)
-hmax.insert(5)
-print(hmax)
-print(hmax.extract())
-print(hmax)
+if __name__ == '__main__':
+    test()
